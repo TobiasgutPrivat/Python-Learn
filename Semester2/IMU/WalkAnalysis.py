62ad51cde1844f365412625f92f2bc724fddd8c6
Poliakov Viktor (poliavik) <poliavik@students.zhaw.ch>
2025-05-06 12:01:12 +0200

    new func analyze

--- Semester2/IMU/WalkAnalysis.py
+++ Semester2/IMU/WalkAnalysis.py
@@ -3,6 +3,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 #TODO: functionality to analyze the Walk data
+def analyze():
+    pass
 
 if __name__ == "__main__":
     path = "GangLisa Konf 1.csv"
