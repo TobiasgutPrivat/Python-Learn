1b29c8f1c095b8e303dc3a5462c8331321a38126
TobiasgutPrivat <tobias.gut.03@gmail.com>
2025-05-08 14:15:49 +0200

    sudoku improved structure

--- Games/Sudoku/Sudoku.py
+++ Games/Sudoku/Sudoku.py
@@ -4,12 +4,10 @@ class Cell:
     '''
     A cell in the sudoku board
     '''
-    maxValue: int 
     possibleValues: list[int]
 
-    def __init__(self, maxValue: int):
-        self.maxValue
-        self.possibleValues = [i for i in range(1, maxValue + 1)] # 1 to maxValue inclusive
+    def __init__(self, possibleValues: list[int]):
+        self.possibleValues = possibleValues
     
     def removePossibility(self, value: int):
         if value in self.possibleValues:
@@ -24,17 +22,20 @@ class FullGroup:
     '''
     A Group of cells containg all values once
     '''
-    values: list
+    values: list[int]
     cells: list[Cell]
     possibleValuePositions: dict[int,list]
-    def __init__(self, cells: list[Cell], values):
+
+    def __init__(self, cells: list[Cell], values: list[int]):
         self.cells = cells
+        self.values = values
     
 class Sudoku:
     '''
     A sudoku board
     '''
     size: int
+    values: list[int]
     board: list[list[Cell]]
     groups: list[FullGroup]
 
@@ -43,26 +44,28 @@ class Sudoku:
             raise ValueError("Size must be a perfect square")
         
         self.size = size
-        self.board = [[Cell(size) for _ in range(size)] for _ in range(size)]
-        self.groups = [FullGroup(row) for row in self.board] # Placeholder for groups
-        self.groups += [FullGroup([self.board[i][j] for i in range(size)]) for j in range(size)]
-        self.groups += [FullGroup([self.board[i][j] for i in range(x, x + int(math.sqrt(size))) for j in range(y, y + int(math.sqrt(size)))]) for x in range(0, size, int(math.sqrt(size))) for y in range(0, size, int(math.sqrt(size)))]
+        self.values = [i for i in range(1, size + 1)]
+
+        self.board = [[Cell(self.values.copy()) for _ in range(size)] for _ in range(size)]
+
+        self.groups = [FullGroup(row,self.values) for row in self.board]
+        self.groups += [FullGroup([self.board[i][j] for i in range(size)],self.values) for j in range(size)]
+        self.groups += [FullGroup([self.board[i][j] for i in range(x, x + int(math.sqrt(size))) for j in range(y, y + int(math.sqrt(size)))],self.values) for x in range(0, size, int(math.sqrt(size))) for y in range(0, size, int(math.sqrt(size)))]
 
     def set_Cell(self, x, y, value):
         self.board[x][y].possibleValues = [value]
 
-    def __str__(self):
-        result = ""
+    def print(self):
         for row in self.board:
-            result += " ".join(str(cell.get_Value()) if cell.get_Value() else "." for cell in row) + "\n"
-        result += "\n"
+            print(" ".join([str(cell.get_Value() or "-") for cell in row]))
+        print()
+        print("Groups:")
         for group in self.groups:
-            result += " ".join(str(cell.get_Value()) if cell.get_Value() else "." for cell in group.cells) + "\n"
-        return result
+            print(" ".join([str(cell.get_Value() or "-") for cell in group.cells]))
 
 if __name__ == "__main__":
     sudoku = Sudoku(9)
     sudoku.set_Cell(0, 0, 5)
     sudoku.set_Cell(1, 1, 3)
     sudoku.set_Cell(2, 2, 4)
-    print(sudoku)
+    sudoku.print()
